459c9e0b5d87170423d80b73582f5ef8759e5c9a
Froz-Ferre <alexej.ferre@gmail.com>
2018-03-24 13:06:10 +0200

    firebase dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,17 @@ import { TodoService } from './services/todo.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TodoInputComponent } from './components/todo-input/todo-input.component';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
+
+import { environment } from '../environments/environment';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +23,9 @@ import { TodoItemComponent } from './components/todo-item/todo-item.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
   providers: [TodoService],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAKPq7vMEEdJbNUeabUwm53W9yZGoYSZuY",
+    authDomain: "todo-app-826d3.firebaseapp.com",
+    databaseURL: "https://todo-app-826d3.firebaseio.com",
+    projectId: "todo-app-826d3",
+    storageBucket: "",
+    messagingSenderId: "477263534137"
+  }
 };
